eb000dad9be619e455a10f3392f59bb9efa50c0d
teerayooti <teerayoot5056@gmail.com>
2022-08-10 14:52:13 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlotHoleDesignComponent } from './example/plot-hole-design/plot-hole-design.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: LandingPageComponent }
+  { path: 'home', component: LandingPageComponent },
+  { path: 'example/plot-hole', component: PlotHoleDesignComponent },
 ];
 
 @NgModule({
